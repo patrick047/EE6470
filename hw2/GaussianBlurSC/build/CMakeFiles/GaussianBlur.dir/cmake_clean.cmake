4acbf06bb13b54967b44e57aeffacca0ebe43e66
patrick047 <72779847+patrick047@users.noreply.github.com>
2021-03-23 03:46:45 +0800

    Add files via upload

---
+++ hw2/GaussianBlurSC/build/CMakeFiles/GaussianBlur.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GaussianBlur.dir/GaussianBlurFilter.cpp.o"
+  "CMakeFiles/GaussianBlur.dir/Testbench.cpp.o"
+  "CMakeFiles/GaussianBlur.dir/main.cpp.o"
+  "GaussianBlur"
+  "GaussianBlur.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GaussianBlur.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
